d17023caef3a00a5dc3d1a05f5c315176e61717f
Johonatan Seibel <globalsys.johonatan@willbank.com.br>
2022-09-13 10:12:26 -0300

    First commit

---
+++ src/main/java/com/example/searchclient/SearchClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/searchclient/SearchClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
